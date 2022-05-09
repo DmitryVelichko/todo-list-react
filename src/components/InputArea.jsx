74d6432843286b621a9d0ae470f138bdf97fa768
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-09 18:07:35 +0300

    new commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+
+
+export default InputArea;
