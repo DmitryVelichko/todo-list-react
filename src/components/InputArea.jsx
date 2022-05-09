b967fc5136f4031c74bd335c415e49ff3b05b159
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-09 18:07:40 +0300

    new commit

--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -8,6 +8,19 @@ function InputArea(props) {
     setInputText(newValue);
   }
 
-
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        onClick={() => {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
 
 export default InputArea;
